46e1eaef58eb0875b3b67458c36b1f67266cda90
Cleiton Gonçalves de Almeida <cgalmeida@azi.com.br>
2018-05-14 19:29:14 -0400

    criação do Resource da Categoria

--- src/main/java/br/com/ufms/compras/resource/CategoriaResource.java
+++ src/main/java/br/com/ufms/compras/resource/CategoriaResource.java
@@ -5,6 +5,7 @@ import br.com.ufms.compras.exception.StandartError;
 import br.com.ufms.compras.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/categorias")
+@CrossOrigin("http://localhost:8080")
 public class CategoriaResource {
 
     @Autowired
